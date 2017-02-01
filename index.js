8f0d64bbc8205ecb3aa0116ae61d1a4d98fbe875
Henrique Aveiro <henrique.aveiro@isobar.com.br>
2017-02-01 15:32:05 -0200

    Documentation and script update

--- index.js
+++ index.js
@@ -1,191 +1,196 @@
-function getElm(sel, all, key, searchIn){
-    searchIn = typeof searchIn == 'string' ? getElm(searchIn) : searchIn;
-    searchIn = (!sel.match(/^#/) && searchIn) || document;
-    if(sel.match(/^#/)){
-        return searchIn.getElementById(sel.replace(/^#/, ''));
-    } else if (sel.match(/^\./)){
-        var el = searchIn.getElementsByClassName(sel.replace(/^\./, ''));
-        return all ? el : el[key || 0];
-    } else {
-        var el = searchIn.getElementsByTagName(sel)
-        return all ? el : el[key || 0];
+'use strict';
+
+function init(){
+
+    function getElm(sel, all, key, searchIn){
+        searchIn = typeof searchIn == 'string' ? getElm(searchIn) : searchIn;
+        searchIn = (!sel.match(/^#/) && searchIn) || document;
+        if(sel.match(/^#/)){
+            return searchIn.getElementById(sel.replace(/^#/, ''));
+        } else if (sel.match(/^\./)){
+            var el = searchIn.getElementsByClassName(sel.replace(/^\./, ''));
+            return all ? el : el[key || 0];
+        } else {
+            var el = searchIn.getElementsByTagName(sel)
+            return all ? el : el[key || 0];
+        }
     }
-}
 
-function toggleClass(elms, c){
-    elms = Array.isArray(elms) || $isNodeList(elms) ? elms : [elms];
-    c = c.split(' ');
-    for(var i = 0, lgt = elms.length; i < lgt; i++){
-        var elmClass = elms[i].classList
-        ,   limit = c.length;
-        while(limit--){elmClass.toggle(c[limit])};
+    function toggleClass(elms, c){
+        elms = Array.isArray(elms) || $isNodeList(elms) ? elms : [elms];
+        c = c.split(' ');
+        for(var i = 0, lgt = elms.length; i < lgt; i++){
+            var elmClass = elms[i].classList
+            ,   limit = c.length;
+            while(limit--){elmClass.toggle(c[limit])};
+        }
     }
-}
 
-function hasClass(elm, c){
-    return $testString(c, elm.className);
-}
-
-function addClass(elms, c){
-    elms = Array.isArray(elms) || $isNodeList(elms) ? elms : [elms];
-    c = c.split(' ');
-    for(var i = 0, lgt = elms.length; i < lgt; i++){
-        var limit = c.length;
-        while(limit--){elms[i].classList.add(c[limit])};
+    function hasClass(elm, c){
+        return $testString(c, elm.className);
     }
-}
 
-function removeClass(elms, c){
-    elms = Array.isArray(elms) || $isNodeList(elms) ? elms : [elms];
-    c = c.split(' ');
-    for(var i = 0, lgt = elms.length; i < lgt; i++){
-        var limit = c.length;
-        while(limit--){elms[i].classList.remove(c[limit])};
+    function addClass(elms, c){
+        elms = Array.isArray(elms) || $isNodeList(elms) ? elms : [elms];
+        c = c.split(' ');
+        for(var i = 0, lgt = elms.length; i < lgt; i++){
+            var limit = c.length;
+            while(limit--){elms[i].classList.add(c[limit])};
+        }
     }
-}
 
-function parseForm(elms){
-    elms = (typeof elms == 'string') ? getElm(elms) : elms;
-    var data = {}
-    ,   inputs = $htmlColToArray(getElm('input', true, false, elms))
-    ,   selects = $htmlColToArray(getElm('select', true, false, elms))    
-    ,   textareas = $htmlColToArray(getElm('textarea', true, false, elms))
-    ,   all = inputs.concat(selects.concat(textareas));
-    
-    for (var i = 0, lgt = all.length; i < lgt; i++){
-        var elm = all[i];
-        if(!elm.name) continue;
-        if(data[elm.name]){
-            if(!elm.checked) continue;
-            if(typeof data[elm.name] == 'string'){
-                data[elm.name] = [data[elm.name], elm.value];
-            } else {
-                data[elm.name].push(elm.value);
+    function removeClass(elms, c){
+        elms = Array.isArray(elms) || $isNodeList(elms) ? elms : [elms];
+        c = c.split(' ');
+        for(var i = 0, lgt = elms.length; i < lgt; i++){
+            var limit = c.length;
+            while(limit--){elms[i].classList.remove(c[limit])};
+        }
+    }
+
+    function parseForm(elms){
+        elms = (typeof elms == 'string') ? getElm(elms) : elms;
+        var data = {}
+        ,   inputs = $htmlColToArray(getElm('input', true, false, elms))
+        ,   selects = $htmlColToArray(getElm('select', true, false, elms))    
+        ,   textareas = $htmlColToArray(getElm('textarea', true, false, elms))
+        ,   all = inputs.concat(selects.concat(textareas));
+        
+        for (var i = 0, lgt = all.length; i < lgt; i++){
+            var elm = all[i];
+            if(!elm.name) continue;
+            if(data[elm.name]){
+                if(!elm.checked) continue;
+                if(typeof data[elm.name] == 'string'){
+                    data[elm.name] = [data[elm.name], elm.value];
+                } else {
+                    data[elm.name].push(elm.value);
+                }
+                continue;
             }
-            continue;
+            if((elm.type == 'checkbox' || elm.type == 'radio') && !elm.checked) continue;
+            data[elm.name] = elm.value;
         }
-        if((elm.type == 'checkbox' || elm.type == 'radio') && !elm.checked) continue;
-        data[elm.name] = elm.value;
-    }
-    return data;
-}
-
-function byAttr(attr, value, elms){
-    elms = elms || document;
-    var search = elms.getElementsByTagName('*')
-    ,   found = [];
-
-    for (var i = 0, lgt = search.length, cur = search[i]; i < lgt; i++, cur = search[i]) {
-        if(!cur.attributes[attr]) continue;
-        if(value && cur.attributes[attr].value != value) continue;
-        found.push(cur);
+        return data;
     }
 
-    return found.length == 1 ? found[0] : found;
-}
+    function byAttr(attr, value, elms){
+        elms = elms || document;
+        var search = elms.getElementsByTagName('*')
+        ,   found = [];
 
-function bindElm(elms, evt, callback){
-    if(typeof elms == 'string') elms = getElm(elms, true);
-    evt = evt.split(' ');
-    elms = Array.isArray(elms) || $isNodeList(elms) ? elms : [elms];
-    for(var i = 0, lgt = elms.length; i < lgt; i++){
-        for(var j = 0, jlgt = evt.length; j < jlgt; j++){
-            elms[i].addEventListener(evt[j], callback);
+        for (var i = 0, lgt = search.length, cur = search[i]; i < lgt; i++, cur = search[i]) {
+            if(!cur.attributes[attr]) continue;
+            if(value && cur.attributes[attr].value != value) continue;
+            found.push(cur);
         }
+
+        return found.length == 1 ? found[0] : found;
     }
-}
 
-function setStyleProp(elm, property, value, time, onFinish){
-    var oProp = {}
-    typeof property === 'object' && (oProp = property) || (oProp[property] = value);
-    for (var stprop in oProp) {
-        elm.style[stprop] = oProp[stprop];
-    }
-    (onFinish || time) && (elm.className += ' animation-happening') && setTimeout(function(){
-        removeClass(elm,'animation-happening');
-        (onFinish || time)(elm);
-    }, typeof onFinish === 'function' ? time : value);
-}
-
-function getUrlParam(url){
-    url = url || window.location.href;
-    
-    if(typeof url !== 'string') throw new Error('Url must be a string!');    
-
-    var spl = url.split('?');
-
-    url = (spl.length === 1) ? spl[0] : spl[1];
-    var param  = {}
-    ,   params = url.split('&');
-
-    for (var i = 0, lgt = params.length; i < lgt; i++) {
-        var splitParam = params[i].split('=')
-        ,   curKey   = splitParam[0]
-        ,   curValue = splitParam[1];
-    
-        if(!curKey) continue;
-
-        if (param[curKey]) {
-            if (!Array.isArray(param[curKey])){
-                param[curKey] = [ param[curKey] ];
+    function bindElm(elms, evt, callback){
+        if(typeof elms == 'string') elms = getElm(elms, true);
+        evt = evt.split(' ');
+        elms = Array.isArray(elms) || $isNodeList(elms) ? elms : [elms];
+        for(var i = 0, lgt = elms.length; i < lgt; i++){
+            for(var j = 0, jlgt = evt.length; j < jlgt; j++){
+                elms[i].addEventListener(evt[j], callback);
             }
-            param[curKey].push(curValue || '');
-            continue;
         }
-        param[curKey] = curValue || '';
     }
-    return param;
-}
 
-function remove(elms,start){
-    if(typeof elms == 'string') elms = getElm(elms,true);
-    elms = Array.isArray(elms) || $isNodeList(elms) ? elms : [elms];
-    start = start || 0;
-    count = ((elms.length - start) >= 0) ? (elms.length - start) : -1;
-    for(var i = 0, lgt = count; i < lgt; i++){
-        elms[start].parentNode.removeChild(elms[start]);
+    function setStyleProp(elm, property, value, time, onFinish){
+        var oProp = {}
+        typeof property === 'object' && (oProp = property) || (oProp[property] = value);
+        for (var stprop in oProp) {
+            elm.style[stprop] = oProp[stprop];
+        }
+        (onFinish || time) && (elm.className += ' animation-happening') && setTimeout(function(){
+            removeClass(elm,'animation-happening');
+            (onFinish || time)(elm);
+        }, typeof onFinish === 'function' ? time : value);
     }
-}
 
-function $testString(filter, string){
-    return (new RegExp('^'+filter+'$|^'+filter+' | '+filter+'$| '+filter+' ')).test(string)
-}
+    function getUrlParam(url){
+        url = url || window.location.href;
+        
+        if(typeof url !== 'string') throw new Error('Url must be a string!');    
 
-function $isNodeList(nodes) {
-    var stringRepr = Object.prototype.toString.call(nodes);
+        var spl = url.split('?');
 
-    return typeof nodes === 'object' &&
-        /^\[object (HTMLCollection|NodeList|Object)\]$/.test(stringRepr) &&
-        (typeof nodes.length === 'number') &&
-        (nodes.length === 0 || (typeof nodes[0] === "object" && nodes[0].nodeType > 0));
-}
+        url = (spl.length === 1) ? spl[0] : spl[1];
+        var param  = {}
+        ,   params = url.split('&');
 
-function $htmlColToArray(collection){
-    if(collection && collection.length == 0) return [];
-    if(collection && !collection.length) return [collection];
-    var elms = [];
-    for(var i = 0, lgt = collection.length; i < lgt; i++){
-        elms.push(collection[i]);
+        for (var i = 0, lgt = params.length; i < lgt; i++) {
+            var splitParam = params[i].split('=')
+            ,   curKey   = splitParam[0]
+            ,   curValue = splitParam[1];
+        
+            if(!curKey) continue;
+
+            if (param[curKey]) {
+                if (!Array.isArray(param[curKey])){
+                    param[curKey] = [ param[curKey] ];
+                }
+                param[curKey].push(curValue || '');
+                continue;
+            }
+            param[curKey] = curValue || '';
+        }
+        return param;
     }
-    return elms;
-}
 
-var domManipulator = {
-    _                  : getElm,
-    _toggleClass       : toggleClass,
-    _addClass          : addClass,
-    _removeClass       : removeClass,
-    _hasClass          : hasClass,
-    _parseForm         : parseForm,
-    _byAttr            : byAttr,
-    _bindElm           : bindElm,
-    _setStyleProp      : setStyleProp,
-    _getUrlParam       : getUrlParam
+    function remove(elms,start){
+        if(typeof elms == 'string') elms = getElm(elms,true);
+        elms = Array.isArray(elms) || $isNodeList(elms) ? elms : [elms];
+        start = start || 0;
+        count = ((elms.length - start) >= 0) ? (elms.length - start) : -1;
+        for(var i = 0, lgt = count; i < lgt; i++){
+            elms[start].parentNode.removeChild(elms[start]);
+        }
+    }
+
+    function $testString(filter, string){
+        return (new RegExp('^'+filter+'$|^'+filter+' | '+filter+'$| '+filter+' ')).test(string)
+    }
+
+    function $isNodeList(nodes) {
+        var stringRepr = Object.prototype.toString.call(nodes);
+
+        return typeof nodes === 'object' &&
+            /^\[object (HTMLCollection|NodeList|Object)\]$/.test(stringRepr) &&
+            (typeof nodes.length === 'number') &&
+            (nodes.length === 0 || (typeof nodes[0] === "object" && nodes[0].nodeType > 0));
+    }
+
+    function $htmlColToArray(collection){
+        if(collection && collection.length == 0) return [];
+        if(collection && !collection.length) return [collection];
+        var elms = [];
+        for(var i = 0, lgt = collection.length; i < lgt; i++){
+            elms.push(collection[i]);
+        }
+        return elms;
+    }
+
+    return {
+        getElm       : getElm,
+        toggleClass  : toggleClass,
+        addClass     : addClass,
+        removeClass  : removeClass,
+        hasClass     : hasClass,
+        parseForm    : parseForm,
+        byAttr       : byAttr,
+        bindElm      : bindElm,
+        setStyleProp : setStyleProp,
+        getUrlParam  : getUrlParam
+    }
 }
 
 try {
-    module.exports = domManipulator;
+    module.exports = init();
 } catch(err){
     // using like a library
-    window.domManipulator = domManipulator;
+    window.dm = init();
 }
--- readme.md
+++ readme.md
@@ -3,56 +3,74 @@ The Dom Manipulator allows you to manipulate HTML elements simply and objectivel
 
 ## Using Dom Manipulator you can:
   - Find elements using the class, id, tagname or attribute;
-  - Set new class or remove a class
-  - Obtain a object by a parsing a form element.
+  - Set new class or remove a class;
+  - Obtain a object using parse form function.
 
 ### Installation
+#### Using like a library
 ```sh
 $ git clone https://github.com/mpahenrique/dom-manipulator.git
-$ cd dom-manipulator
-$ npm install
+```
+```HTML
+<body>
+...
+...
+<script src="path/to/dom-manipulator/index.js"></script>
+</body>
+```
+
+#### Using like a module
+```sh
+$ npm install dom-manipulator --save
+```
+
+### Usage (Getting Dom Manipulator method)
+#### Using like a library
+```js
+  dm.somedmMethod('#my-element'); // When using like a library dm is global (window.dm)
 ```
 
 ### Usage
+#### Using like a module
+```js
+var dm = require('dom-manipulator');
+```
+
+### Methods
 ```js
 'use strict';
 
-var domManipulator = require('dom-manipulator');
-
-var _elm  = domManipulator._('theElement')
+var _elm  = dm.getElm('#my-element')
 // ==> Get the element {optional: ['all'(boolean: return all selectors), 'key'(integer: select the position), 'searchIn'(string: search the element inside a node)]}
 
-var _form = domManipulator._('form');
-
-domManipulator._toggleClass(_elm,'classToggle')
+dm.toggleClass(_elm,'classToggle')
 // ==> This function, verify if 'classToToggle' existy, in true case, they remove the class, otherwise add the class
 
-domManipulator._hasClass(_elm,'verifyClass')
+dm.hasClass(_elm,'verifyClass')
 // ==> Return a boolean if the element has class
 
-domManipulator._addClass(_elm,'classRemove')
+dm.addClass(_elm,'classRemove')
 // ==> Add the class
 
-domManipulator._removeClass(_elm,'classRemove')
+dm.removeClass(_elm,'classRemove')
 // ==> Remove the class
 
-domManipulator._parseForm(_form)
+dm.parseForm('#form-element')
 // ==> Return a object with name (as key) and the value of elements
 
-domManipulator._byAttr('tabindex')
+dm.byAttr('tabindex')
 // ==> Return elements with the atrribute. {optional: ['value'(string: search the attribute with the same value), 'elms(DOMObject: search elements with the attribute in a element father)']}
 
-domManipulator._bindElm(_elm, 'click', function(){console.log('click')})
+dm.bindElm(_elm, 'click', callback)
 // ==> Binding a element with a event
 
-domManipulator._setStyleProp(_elm, 'border', '20px solid red')
+dm.setStyleProp(_elm, 'border', '20px solid red')
 // ==> Setting a style inline to element and you cam pass the style properties as a Object, like as: {'border': '2px solid yellow', 'margin-top': '20px', 'overflow':'auto'}. {optional: ['time'(integer: pass a time, in milliseconds, to call the callback), 'onFinish'(Function: A Callback)]}
 
-domManipulator._getUrlParam()
+dm.getUrlParam()
 // ==> Get the url params and return as a Object {optional: ['url'(string: pass a url with params to get the Object return, by default they get window.location)]}
 
-domManipulator.remove(_._('theElement'))
+dm.remove(_elm) /* or */ dm.remove('.my-other-element');
 // ==> Remove the elements {optional: ['start'(Integer: Pass the index to start the remove itens)]}
 
 ```
-
